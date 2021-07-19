5952483ca634c8ae2a1a5cd6cff1020a4321ea88
TudorAnitas <anitas_tudor@yahoo.com>
2021-07-19 14:46:34 +0300

    initial commit

---
+++ test_project/lib/screens/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: FutureBuilder(
+          future: http,
+          builder: (context, snapshot) {},
+        ),
+      ),
+    );
+  }
+}
